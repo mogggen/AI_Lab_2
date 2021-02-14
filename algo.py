9d11f523be0a251b28f94bc562569b304c9cd915
Morgan Nyman <mornym-9@student.ltu.se>
2021-02-14 20:30:10 +0100

    custom algo and Finish A*, optimus out

--- algo.py
+++ algo.py
@@ -1,8 +1,8 @@
 import turtle
 
-s = 5
+s = 10
 
-def rect(p, place="", size = s):
+def rect(p):
     tur = turtle.Turtle()
     
     #setters
@@ -11,7 +11,7 @@ def rect(p, place="", size = s):
     tur.speed(0)
     for i in p:
         if i[2] in (" ", "0"): continue
-        x = i[0] * s + s
+        x = i[0] * s
         y = i[1] * -s
         c = i[2]
         
@@ -19,52 +19,58 @@ def rect(p, place="", size = s):
         tur.fillcolor("gray")
 
         #position
-        tur.goto(x - size / 2, y - size / 2)
+        tur.goto(x - s / 2, y - s / 2)
 
         #fill rec
         tur.begin_fill()
         
-        tur.setx(x + size / 2)
-        tur.sety(y + size / 2)
-        tur.setx(x - size / 2)
-        tur.sety(y - size / 2)
-        tur.end_fill()
-
-    if place:
-        tur.write(place)
+        tur.setx(x + s / 2)
+        tur.sety(y + s / 2)
+        tur.setx(x - s / 2)
+        tur.sety(y - s / 2)
+        tur.end_fill()    
 
 
 
 #flags
 Found = False
-counter = 0
+
+visited = []
+
 queue = []
+
+opened = []
+closed = []
 
 def resetFlags():
     global Found
-    global counter
+    global visited
     global queue
+    global opened
+    global closed
+    
     Found = False
-    counter = 0
+
+    visited = []
     queue = []
+    opened = []
+    closed = []
     
     
 
-def dfs(visited, graph, node):
+def dfs(graph, node):
     global Found
-    global counter
-    counter += 1
     if graph[node][0] == "G": Found = True
     if graph[node][0] == "X" or Found: return
-    rect([(node[0], node[1], "T")])
+    #rect([(node[0], node[1], "T")])
     if node not in visited:
         visited.append(node)
         
         for neighbour in graph[node][1:]:
-            dfs(visited, graph, neighbour)
-    return #len(visited)
+            dfs(graph, neighbour)
+    return
 
-def bfs(visited, graph, node):
+def bfs(graph, node):
 
     visited.append(node)
     queue.append(node)
@@ -76,5 +82,73 @@ def bfs(visited, graph, node):
                 visited.append(neighbour)
                 if graph[neighbour][0] != "X":
                     queue.append(neighbour)
-                if graph[neighbour][0] == "G": return #len(visited)
-                rect([(neighbour[0], neighbour[1], "T")])
+                if graph[neighbour][0] == "G": return
+                #rect([(neighbour[0], neighbour[1], "T")])
+
+def astar(graph, node, fin):
+    opened.append(node)
+
+    # Loop until you find the end
+    while len(opened) > 0:
+        
+        for item in opened:
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        opened.pop(current_index)
+        closed.append(current_node)
+
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        children = []
+        for new_position in [(1, 1), (0, 1), (1, 0), (-1, 1), (1, -1), (-1, 0), (0, -1), (-1, -1)]: # Adjacent squares
+
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            new_node = Node(current_node, node_position)
+
+            children.append(new_node)
+
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = (child.position[0] - end_node.position[0]) ** 2 + (child.position[1] - end_node.position[1]) ** 2
+            child.f = child.g + child.h
+
+            # A better child is already in the open list
+            for open_node in opened:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            opened.append(child)
+
+def custom(graph, node):
+    global Found
+    if node not in ((0, -1), (0, 1), (-1, 0), (1, 0)): return
+    if graph[node][0] == "G": Found = True
+    if graph[node][0] == "X" or Found: return
+    rect([(node[0], node[1], "T")])
+    if node not in visited:
+        visited.append(node)
+        
+        for neighbour in graph[node][1:]:
+            if node not in ((node[0], node[1] - 1), (node[0], node[1] + 1), (node[0] - 1, node[1]), (node[0] + 1, node[1])):
+            dfs(graph, neighbour)
+    return
--- main.py
+++ main.py
@@ -12,28 +12,25 @@ def rect(p):
     tur.ht()
     tur.up()
     tur.speed(0)
-    for i in p:
-        #print(i)
+    for i in p:        
         if i[2] in (" ", "0"): continue
         x = i[0]
         y = i[1]
         c = i[2]
-        w = i[3]
-        h = i[4]
         
         #continue
         tur.fillcolor(getColor(c))
 
         #position
-        tur.goto(x - h / 2, y - h / 2)
+        tur.goto(x - s / 2, y - s / 2)
 
         #fill rec
         tur.begin_fill()
         
-        tur.setx(x + w / 2)
-        tur.sety(y + h / 2)
-        tur.setx(x - w / 2)
-        tur.sety(y - h / 2)
+        tur.setx(x + s / 2)
+        tur.sety(y + s / 2)
+        tur.setx(x - s / 2)
+        tur.sety(y - s / 2)
         tur.end_fill()
 
 foo = open("Map1.txt", 'r')
@@ -59,25 +56,15 @@ def drawMap(karta):
     nodes = []
     x = 0
     y = 0
-    last = ''
-    w = 0
     
     for c in karta:
-        print(last,end='')
-        if last != c:
-            if c == '\n':
-                nodes += [(x, y, last, w, s)]
-                x = 0
-                w = 0
-                y -= s
-                last = c
-            else:
-                nodes += [(x, y, c, w, s)]
-                last = c
-                x = x + w
-                w = 0
+        if c == '\n':
+            x = 0
+            y -= s
         else:
-            w += s       
+            nodes += [(x, y, c)]
+            x += s
+        
     rect(nodes)
     return nodes
 
@@ -101,7 +88,7 @@ def connectGraph(graph):
     for g in graph:
         for n in r:
             try:
-                if graph[g[0] + n[0], g[1] + n[1]][0] in ('0', ' ', 'S', 'G'):
+                if "X" not in (graph[g[0] + n[0], g[1]][0], graph[g[0], g[1] + n[1]][0]) and graph[g[0] + n[0], g[1] + n[1]][0] in ('0', ' ', 'S', 'G'):
                     graph[g] += [(g[0] + n[0], g[1] + n[1])]
             except KeyError:
                 continue
@@ -109,30 +96,69 @@ def connectGraph(graph):
 
 def drawfunc(karta, func):
     tur = turtle.Turtle()
-    tur.screen.clear()    
+    tur.screen.clear()
     drawMap(karta)
     g = makeGraph(karta)
     g = connectGraph(g)
 
-    visited = []
     algo.resetFlags()
 
     #find S
     for v in g:
         if g[v][0] == "S":
             if func == "dfs":
-                algo.dfs(visited, g, v)
+                algo.dfs(g, v)
             elif func == "bfs":
-                algo.bfs(visited, g, v)
-    tur.write("Press 'Enter' in console to contiue...")
+                algo.bfs(g, v)
+            elif func == "A*":
+                for vv in g:
+                    if g[vv][0] == "G":
+                        algo.astar(g, v, vv)
 
-def timefunc(karta, func, itr = 1000):    
-    print(func)
-
-#driver code
-for i in kartor: #this needs to be an array, not a index
-    drawfunc(i, "dfs")
-    drawfunc(i, "bfs")
+def timefunc(karta, func, itr = 1000):
+    g = makeGraph(karta)
+    g = connectGraph(g)
     
-    #timefunc(i, "dfs")
-    #timefunc(i, "bfs")
+    algo.resetFlags()
+    
+    #find S
+    for v in g:
+        if g[v][0] == "S":
+            if func == "dfs":
+                start = time.time()
+                algo.dfs(g, v)
+                return time.time() - start
+            elif func == "bfs":
+                start = time.time()
+                algo.bfs(g, v)
+                return time.time() - start
+            elif func == "A*":
+                for vv in g:
+                    if g[vv][0] == "G":
+                        start = time.time()
+                        algo.astar(g, v, vv)
+                        return time.time() - start
+#driver code
+for i in enumerate(kartor):
+    print("Map" + str(i[0] + 1) + ".txt")
+    #drawfunc(i[1], "dfs")
+    #drawfunc(i[1], "bfs")
+    #drawfunc(i[1], "A*")
+    #drawfunc(i[1], "custom")
+    
+    dfsTime = 0
+    bfsTime = 0
+    AstarTime = 0
+    customTime = 0
+
+    for itr in range(4000):
+        pass
+        dfsTime += timefunc(i[1], "dfs")
+        bfsTime += timefunc(i[1], "bfs")
+        #AstarTime += timefunc(i[1], "A*")
+        customTime += timefunc(i[1], "custom")
+
+    print("dfs:", dfsTime / 1000)
+    print("bfs:", bfsTime / 1000)
+    #print("A*:", AstarTime / 1000)
+    print("custom:", customTime / 1000)
